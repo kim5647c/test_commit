6463aae00640cef986b5041a255fe272552c4793
kim5647 <stepanv2005cc@gmail.com>
2023-11-02 15:53:40 +0300

    5commit

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,25 +3,45 @@
 
 using namespace std;
 
-
 const unsigned int DIM1 = 3;
 const unsigned int DIM2 = 5;
 const unsigned int DIM3 = 2;
 
-int ary[DIM1][DIM2][DIM3];
-
 int main() {
 
+    int*** ary;
+
+    int cnt = 1;
+
+    // создание
+    ary = new int** [DIM1];
+    for (int i = 0; i < DIM1; i++) {
+        ary[i] = new int* [DIM2];
+        for (int j = 0; j < DIM2; j++) {
+            ary[i][j] = new int[DIM3];
+        }
+    }
+
+    // работа с массивом
     for (int i = 0; i < DIM1; i++) {
         for (int j = 0; j < DIM2; j++) {
             for (int k = 0; k < DIM3; k++) {
-                ary[i][j][k] = (i + 1) * 100 + (j + 1) * 10 + (k + 1);
+                ary[i][j][k] = cnt++;
                 cout << setw(4) << ary[i][j][k];
             }
             cout << endl;
         }
         cout << endl;
     }
+
+    // уничтожение
+    for (int i = 0; i < DIM1; i++) {
+        for (int j = 0; j < DIM2; j++) {
+            delete[] ary[i][j];
+        }
+        delete[] ary[i];
+    }
+    delete[] ary;
 
     return 0;
 }
