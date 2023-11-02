5456250342c5be7b75b9d9f0396aff2e4fd40537
kim5647 <stepanv2005cc@gmail.com>
2023-11-02 15:47:00 +0300

    2commit

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -7,3 +7,15 @@ const unsigned int DIM1 = 3;
 const unsigned int DIM2 = 5;
 
 int ary[DIM1][DIM2];
+
+int main()
+{
+
+    for (int i = 0; i < DIM1; i++)
+    {
+        for (int j = 0; j < DIM2; j++)
+        {
+            ary[i][j] = (i + 1) * 10 + (j + 1);
+        }
+    }
+}
