535a57f89e7891858c15f648d1a3670dd996b980
kim5647 <stepanv2005cc@gmail.com>
2023-11-02 15:50:13 +0300

    4commit

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -9,3 +9,19 @@ const unsigned int DIM2 = 5;
 const unsigned int DIM3 = 2;
 
 int ary[DIM1][DIM2][DIM3];
+
+int main() {
+
+    for (int i = 0; i < DIM1; i++) {
+        for (int j = 0; j < DIM2; j++) {
+            for (int k = 0; k < DIM3; k++) {
+                ary[i][j][k] = (i + 1) * 100 + (j + 1) * 10 + (k + 1);
+                cout << setw(4) << ary[i][j][k];
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
